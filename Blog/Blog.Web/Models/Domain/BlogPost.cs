636af64616bfc5d519d0137ae06ae0fda5f52920
Yasemin-s <l2012721036@isparta.edu.tr>
2024-04-30 06:56:41 +0300

    entities oluşturuldu
    entities oluşturuldu

---
+++ Blog/Blog.Web/Models/Domain/BlogPost.cs
@@ -0,0 +1,17 @@
+﻿namespace Blog.Web.Models.Domain
+{
+    public class BlogPost
+    {
+        public Guid Id { get; set; }
+        public String Heading { get; set; }
+        public String PageTitle { get; set; }
+        public String Content { get; set; }
+        public String ShortDescription { get; set; }
+        public String FeaturedImageUrl { get; set; }
+        public String UrlHandle { get; set; }
+        public DateTime PublishedDate { get; set; }
+        public string Author { get; set; }
+        public bool Visible { get; set; }
+        public ICollection<Tag> Tags { get; set; }
+    }
+}
