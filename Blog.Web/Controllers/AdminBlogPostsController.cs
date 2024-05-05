b258e161c451e33d45d8d23fa4e770d0de291675
Yasemin-s <l2012721036@isparta.edu.tr>
2024-05-05 18:21:25 +0300

    post kısmına geçildi.
    post kısmına geçildi.

---
+++ Blog.Web/Controllers/AdminBlogPostsController.cs
@@ -0,0 +1,46 @@
+﻿using Blog.Web.Models.ViewModels;
+using Blog.Web.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Blog.Web.Controllers
+{
+    public class AdminBlogPostsController : Controller
+    {
+        private readonly ITagRepository tagRepository;
+
+        //post için crud işlemleri yapıcaz ve asenkron bir şekilde metotlar uzerinde calisiyoruz. 
+
+
+        public AdminBlogPostsController(ITagRepository tagRepository)
+        {
+            this.tagRepository = tagRepository;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Add()
+        {
+
+            //etiketleri gostermek icin vt baglantisi gerek
+            var tags = await tagRepository.GetAllAsync();
+
+            var model = new AddBlogPostRequest
+            {
+                Tags = tags.Select(x => new SelectListItem
+                {
+                    Text = x.Name,
+                    Value = x.Id.ToString()
+                })
+            };
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add(AddBlogPostRequest addBlogPostRequest)
+        {
+            return RedirectToAction("Add");
+        }
+
+    }
+}
---
+++ Blog.Web/Models/ViewModels/AddBlogPostRequest.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Blog.Web.Models.ViewModels
+{
+    public class AddBlogPostRequest
+    {
+        public String Heading { get; set; }
+        public String PageTitle { get; set; }
+        public String Content { get; set; }
+        public String ShortDescription { get; set; }
+        public String FeaturedImageUrl { get; set; }
+        public String UrlHandle { get; set; }
+        public DateTime PublishedDate { get; set; }
+        public string Author { get; set; }
+        public bool Visible { get; set; }
+
+        //vt den etiketleri secip ekranda gostermek icin 
+        public IEnumerable<SelectListItem> Tags {  get; set; }
+        //etiketleri yakalamk icin
+        public string SelectedTag {  get; set; }
+    }
+}
