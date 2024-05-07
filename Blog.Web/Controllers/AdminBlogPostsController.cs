e9875648b21f5267dbdb3b6306131d5ad0f5e0dd
Yasemin-s <l2012721036@isparta.edu.tr>
2024-05-07 12:54:41 +0300

    düzenleme için görünüm oluşturuldu.
    düzenleme için görünüm oluşturuldu.

--- Blog.Web/Controllers/AdminBlogPostsController.cs
+++ Blog.Web/Controllers/AdminBlogPostsController.cs
@@ -87,5 +87,44 @@ namespace Blog.Web.Controllers
             return View(blogPosts);
         }
 
+
+        [HttpGet]
+        public async Task<IActionResult> Edit(Guid id)
+        {
+            var blogPost = await blogPostRepository.GetAsync(id);
+            var tagsDomainModel = await tagRepository.GetAllAsync();
+
+            if (blogPost != null)
+            {
+
+                //view modeli domain modeline donusturun
+                var model = new EditBlogPostRequest
+                {
+                    Id = blogPost.Id,
+                    Heading = blogPost.Heading,
+                    PageTitle = blogPost.PageTitle,
+                    Content = blogPost.Content,
+                    Author = blogPost.Author,
+                    FeaturedImageUrl = blogPost.FeaturedImageUrl,
+                    UrlHandle = blogPost.UrlHandle,
+                    ShortDescription = blogPost.ShortDescription,
+                    PublishedDate = blogPost.PublishedDate,
+                    Visible = blogPost.Visible,
+                    Tags = tagsDomainModel.Select(x => new SelectListItem
+                    {
+                        Text = x.Name,
+                        Value = x.Id.ToString(),
+
+                    }),
+                    SelectedTags = blogPost.Tags.Select(x => x.Id.ToString()).ToArray()
+                };
+                return View(model);
+            }
+
+            return View(null);
+        }
+
+
+
     }
 }
--- Blog.Web/Models/ViewModels/EditBlogPostRequest.cs
+++ Blog.Web/Models/ViewModels/EditBlogPostRequest.cs
@@ -1,6 +1,8 @@
-﻿namespace Blog.Web.Models.Domain
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Blog.Web.Models.ViewModels
 {
-    public class BlogPost
+    public class EditBlogPostRequest
     {
         public Guid Id { get; set; }
         public string Heading { get; set; }
@@ -12,6 +14,10 @@
         public DateTime PublishedDate { get; set; }
         public string Author { get; set; }
         public bool Visible { get; set; }
-        public ICollection<Tag> Tags { get; set; }
+
+        // Display tags
+        public IEnumerable<SelectListItem> Tags { get; set; }
+        // Collect Tag
+        public string[] SelectedTags { get; set; } = Array.Empty<string>();
     }
 }
