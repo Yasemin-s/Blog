3a186da053aeb58cdb08438de309d1a880816b20
Yasemin-s <l2012721036@isparta.edu.tr>
2024-05-09 12:24:20 +0300

    home sayfasına kullanılan etiketler genel olarak eklendi.
    home sayfasına kullanılan etiketler genel olarak eklendi.

--- Blog.Web/Controllers/HomeController.cs
+++ Blog.Web/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
 using Blog.Web.Models;
+using Blog.Web.Models.Domain;
+using Blog.Web.Models.ViewModels;
 using Blog.Web.Repositories;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
@@ -9,17 +11,32 @@ namespace Blog.Web.Controllers
     {
         private readonly ILogger<HomeController> _logger;
         private readonly IBlogPostRepository blogPostRepository;
+        private readonly ITagRepository tagRepository;
 
-        public HomeController(ILogger<HomeController> logger, IBlogPostRepository blogPostRepository)
+        public HomeController(ILogger<HomeController> logger, IBlogPostRepository blogPostRepository,
+            ITagRepository tagRepository)
         {
             _logger = logger;
             this.blogPostRepository = blogPostRepository;
+            this.tagRepository = tagRepository;
         }
 
         public async Task<IActionResult> Index()
         {
+
+            //postlari getirme
             var blogPosts = await blogPostRepository.GetAllAsync();
-            return View(blogPosts); 
+
+            //etiketleri getirme
+            var tags = await tagRepository.GetAllAsync();
+
+            var model = new HomeViewModel
+            {
+                BlogPosts = blogPosts,
+                Tags = tags
+            };
+
+            return View(model); 
         }
 
         public IActionResult Privacy()
